bd9808970b948291f757b362c5cd7c81d7776a66
ThorKild_ <pro.antoine.masia@gmail.com>
2024-04-12 11:45:42 +0200

    setup php doc AuthService

--- src/Service/AuthService.php
+++ src/Service/AuthService.php
@@ -5,6 +5,13 @@ use Symfony\Component\HttpFoundation\Request;
 
 class AuthService
 {
+    /**
+     * Checks if a user is connected based on the presence of a 'user_id' key in the session.
+     *
+     * @param Request $request The current request object.
+     *
+     * @return bool True if a user is connected, false otherwise.
+     */
     public function isConnected(Request $request) : bool
     {
         $session = $request->getSession();
